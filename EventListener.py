e8dc4969688980a70434117c1e3e3cd25c7d2f09
Tushar Garud <tg415h@wayfair.com>
2020-05-22 12:51:04 -0400

    Now vector can tell random Chuck Norris jokes

--- EventListener.py
+++ EventListener.py
@@ -5,12 +5,12 @@ import threading
 
 said_text = False
 
-def on_wake_word_custom(robot, event_type, event, evt):
-    print("Vector sees a face")
+def on_robot_observed_face(robot, event_type, event, evt):
+    print(f"Vector sees a face:{evt}\n")
     global said_text
     if not said_text:
         said_text = True
-        robot.behavior.say_text("Whats up")
+        robot.behavior.say_text("I see a face!")
         evt.set()
 
 with anki_vector.Robot(enable_face_detection=True) as robot:
@@ -20,13 +20,14 @@ with anki_vector.Robot(enable_face_detection=True) as robot:
     robot.behavior.set_lift_height(0.0)
 
     evt = threading.Event()
-    robot.events.subscribe(on_wake_word_custom, Events.wake_word, evt)
+    robot.events.subscribe(on_robot_observed_face, Events.robot_observed_face, evt)
 
     print("------ waiting for face events, press ctrl+c to exit early ------")
+
     try:
-        if not evt.wait(timeout=10):
+        if not evt.wait(timeout=120.0):
             print("------ Vector never saw your face! ------")
     except KeyboardInterrupt:
         pass
 
-robot.events.unsubscribe(on_wake_word_custom, Events.wake_word)
+robot.events.unsubscribe(on_robot_observed_face, Events.robot_observed_face)
